12bfff00e3cd4d71f0b1921f5b46a3096e8354bf
CHANDAN PRAKASH <141991237+chandanprakash14@users.noreply.github.com>
2024-12-09 10:40:52 +0530

    Delete scikitlearn pratice directory

--- scikitlearn pratice/splictingfunction.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import train_test_split 
-X=list(range(0,15))
-y=[x*x for x in X]
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, test_size=0.3, random_state=42)
-print(X_train)
-print(X_test)
-print(y_test)
-print(y_train)
